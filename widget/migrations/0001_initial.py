4f13bf3e0d505c4eb598005836b2b80ca44db003
Maciej Żuk <mzuk@milosolutions.com>
2013-09-20 14:45:07 +0200

    migrations

--- widget/migrations/0001_initial.py
+++ widget/migrations/0001_initial.py
@@ -1,61 +1,198 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
-        # Adding model 'WidgetSlot'
-        db.create_table('widget_widgetslot', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('_order', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+        # Adding model 'WidgetModel'
+        db.create_table(u'widget_widgetmodel', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
+            ('widget', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['widget.Widget'])),
         ))
-        db.send_create_signal('widget', ['WidgetSlot'])
+        db.send_create_signal(u'widget', ['WidgetModel'])
 
         # Adding model 'Widget'
-        db.create_table('widget_widget', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'widget_widget', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
             ('_order', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('widget_module', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('active', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='widgets', to=orm['auth.User'])),
+            ('display_title', self.gf('django.db.models.fields.CharField')(default=None, max_length=255)),
+            ('widget_class', self.gf('widget.fields.PageWidgetClass')(default='', max_length=255)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('widget_file_title', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('author', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('acts_on', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('widgetslot', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['widget.WidgetSlot'], null=True)),
+            ('widgetslot', self.gf('django.db.models.fields.CharField')(default='none', max_length=255)),
+            ('page_less', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('page', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['pages.Page'], null=True, blank=True)),
+            ('status', self.gf('django.db.models.fields.IntegerField')(default=1)),
+            ('publish_date', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('expiry_date', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
         ))
-        db.send_create_signal('widget', ['Widget'])
+        db.send_create_signal(u'widget', ['Widget'])
+
+        # Adding model 'WidgetOptionGroup'
+        db.create_table(u'widget_widgetoptiongroup', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
+            ('widget', self.gf('django.db.models.fields.related.OneToOneField')(related_name='option_group', unique=True, to=orm['widget.Widget'])),
+            ('entry_time', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal(u'widget', ['WidgetOptionGroup'])
+
+        # Adding model 'WidgetOptionEntry'
+        db.create_table(u'widget_widgetoptionentry', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
+            ('widget', self.gf('django.db.models.fields.related.ForeignKey')(related_name='options', to=orm['widget.Widget'])),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('value', self.gf('django.db.models.fields.CharField')(max_length=2000)),
+        ))
+        db.send_create_signal(u'widget', ['WidgetOptionEntry'])
 
 
     def backwards(self, orm):
-        
-        # Deleting model 'WidgetSlot'
-        db.delete_table('widget_widgetslot')
+        # Deleting model 'WidgetModel'
+        db.delete_table(u'widget_widgetmodel')
 
         # Deleting model 'Widget'
-        db.delete_table('widget_widget')
+        db.delete_table(u'widget_widget')
+
+        # Deleting model 'WidgetOptionGroup'
+        db.delete_table(u'widget_widgetoptiongroup')
+
+        # Deleting model 'WidgetOptionEntry'
+        db.delete_table(u'widget_widgetoptionentry')
 
 
     models = {
-        'widget.widget': {
-            'Meta': {'ordering': "('_order',)", 'object_name': 'Widget'},
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'generic.assignedkeyword': {
+            'Meta': {'ordering': "('_order',)", 'object_name': 'AssignedKeyword'},
             '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keyword': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'assignments'", 'to': u"orm['generic.Keyword']"}),
+            'object_pk': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'generic.keyword': {
+            'Meta': {'object_name': 'Keyword'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'})
+        },
+        u'pages.page': {
+            'Meta': {'ordering': "('titles',)", 'object_name': 'Page'},
+            '_meta_title': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'content_model': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'expiry_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'gen_description': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_menus': ('mezzanine.pages.fields.MenusField', [], {'default': '(1, 2, 3)', 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'in_sitemap': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'keywords': ('mezzanine.generic.fields.KeywordsField', [], {'object_id_field': "'object_pk'", 'to': u"orm['generic.AssignedKeyword']", 'frozen_by_south': 'True'}),
+            'keywords_string': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
+            'login_required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': u"orm['pages.Page']"}),
+            'publish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'short_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '2'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'titles': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'null': 'True'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'null': 'True'})
+        },
+        u'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'widget.widget': {
+            'Meta': {'ordering': "('display_title',)", 'object_name': 'Widget'},
+            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'acts_on': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'author': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'widget_module': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'widgetslot': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['widget.WidgetSlot']", 'null': 'True'})
+            'display_title': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '255'}),
+            'expiry_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'page': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['pages.Page']", 'null': 'True', 'blank': 'True'}),
+            'page_less': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'publish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'widgets'", 'to': u"orm['auth.User']"}),
+            'widget_class': ('widget.fields.PageWidgetClass', [], {'default': "''", 'max_length': '255'}),
+            'widget_file_title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'widgetslot': ('django.db.models.fields.CharField', [], {'default': "'none'", 'max_length': '255'})
         },
-        'widget.widgetslot': {
-            'Meta': {'ordering': "('_order',)", 'object_name': 'WidgetSlot'},
-            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        u'widget.widgetmodel': {
+            'Meta': {'object_name': 'WidgetModel'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'widget': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['widget.Widget']"})
+        },
+        u'widget.widgetoptionentry': {
+            'Meta': {'object_name': 'WidgetOptionEntry'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'widget': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'options'", 'to': u"orm['widget.Widget']"})
+        },
+        u'widget.widgetoptiongroup': {
+            'Meta': {'object_name': 'WidgetOptionGroup'},
+            'entry_time': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'widget': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'option_group'", 'unique': 'True', 'to': u"orm['widget.Widget']"})
         }
     }
 
